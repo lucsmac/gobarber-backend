c2ab8a9d8285cf050ff55b75db8592817076ac06
Lucas Macedo <lucasmdc57@gmail.com>
2021-01-25 23:11:12 -0300

    modeling folders to DDD

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './routes';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
 
-import './database';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
 
 const app = express();
 
